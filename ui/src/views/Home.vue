abe80770209f0aec36025c791d9592bb19a49439
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-06-09 19:15:47 +0100

    init ui

---
+++ ui/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img width="300" src="https://raw.githubusercontent.com/MariaLetta/free-gophers-pack/master/characters/svg/51.svg">
+    <HelloWorld msg="Hello hot reload!"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
