876005846341499a2f7282cbe67de263cb81d832
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-06-09 19:15:27 +0100

    init lib

---
+++ lib/watchers/network_watcher.go
@@ -0,0 +1,28 @@
+package events
+
+// "github.com/guark/guark"
+
+// type Watcher interface {
+// 	Watch(*guark.App)
+// 	//
+// }
+
+// type guark.SignalWatcher struct {}
+
+// func (SignalWatcher) Value(ctx *guark.Context) os.Signal {
+// 	return ctx.Params["signal"].(os.Signal)
+// }
+
+// func (SignalWatcher) Watch(g *guark.App) {
+// 	// watch here
+// }
+
+// type SignalWatcher struct {
+// 	guark.SignalWatcher
+// }
+
+// func (l SignalListener) Handle(ctx *guark.Context) {
+
+// 	// signal := l.Value(ctx)
+
+// }
