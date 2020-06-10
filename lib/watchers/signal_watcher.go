2181f3514f4556989b2a1f14b31d64cf3a0eea6b
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-06-11 00:01:49 +0100

    clean watchers dir

--- lib/watchers/signal_watcher.go
+++ lib/watchers/signal_watcher.go
@@ -1,28 +1,2 @@
-package events
+package watchers
 
-// "github.com/guark/guark"
-
-// type Watcher interface {
-// 	Watch(*guark.App)
-// 	//
-// }
-
-// type guark.SignalWatcher struct {}
-
-// func (SignalWatcher) Value(ctx *guark.Context) os.Signal {
-// 	return ctx.Params["signal"].(os.Signal)
-// }
-
-// func (SignalWatcher) Watch(g *guark.App) {
-// 	// watch here
-// }
-
-// type SignalWatcher struct {
-// 	guark.SignalWatcher
-// }
-
-// func (l SignalListener) Handle(ctx *guark.Context) {
-
-// 	// signal := l.Value(ctx)
-
-// }
